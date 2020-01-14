85f99a7a9c36d8464c96dde78181c3d5cda805b4
mike881205 <mike881205@gmail.com>
2020-01-13 21:30:55 -0500

    added code to associte user and availability tables.

--- models/availability.js
+++ models/availability.js
@@ -2,40 +2,50 @@ module.exports = function (sequelize, DataTypes) {
     let Availability = sequelize.define("Availability", {
         day1: {
             type: DataTypes.BOOLEAN,
-            defaultValue: 0,
-            flag: {allowNull: false}
+            defaultValue: false,
+            flag: { allowNull: false }
         },
         day2: {
             type: DataTypes.BOOLEAN,
-            defaultValue: 0,
-            flag: {allowNull: false}
+            defaultValue: false,
+            flag: { allowNull: false }
         },
         day3: {
             type: DataTypes.BOOLEAN,
-            defaultValue: 0,
-            flag: {allowNull: false}
+            defaultValue: false,
+            flag: { allowNull: false }
         },
         day4: {
             type: DataTypes.BOOLEAN,
-            defaultValue: 0,
-            flag: {allowNull: false}
+            defaultValue: false,
+            flag: { allowNull: false }
         },
         day5: {
             type: DataTypes.BOOLEAN,
-            defaultValue: 0,
-            flag: {allowNull: false}
+            defaultValue: false,
+            flag: { allowNull: false }
         },
         day6: {
             type: DataTypes.BOOLEAN,
-            defaultValue: 0,
-            flag: {allowNull: false}
+            defaultValue: false,
+            flag: { allowNull: false }
         },
         day7: {
             type: DataTypes.BOOLEAN,
-            defaultValue: 0,
-            flag: {allowNull: false}
+            defaultValue: false,
+            flag: { allowNull: false }
         }
     });
+
+    Availability.associate = function (models) {
+        // We're saying that a Post should belong to an Author
+        // A Post can't be created without an Author due to the foreign key constraint
+        Availability.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
 
     return Availability;
 };
--- models/user.js
+++ models/user.js
@@ -1,8 +1,19 @@
 module.exports = function(sequelize, DataTypes) {
     let User = sequelize.define("User", {
-      name: DataTypes.STRING,
-      flag: {allowNull: false}
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+   
     });
+
+    User.associate = function(models) {
+      // Associating Author with Posts
+      // When an Author is deleted, also delete any associated Posts
+      User.hasMany(models.Availability, {
+        onDelete: "cascade"
+      });
+    };
   
     return User;
   };
