0d1b9362daf38ebdd0cfa066340270f73d308381
mike881205 <mike881205@gmail.com>
2020-01-13 19:55:30 -0500

    changes

--- models/user.js
+++ models/user.js
@@ -1,17 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
-    let Friend = sequelize.define("Friend", {
-      // Giving the Friend model a name of type STRING
-      name: DataTypes.STRING
+    let User = sequelize.define("User", {
+      name: DataTypes.STRING,
+      flag: {allowNull: false}
     });
   
-    Friend.associate = function(models) {
-      // Associating Friend with Posts
-      // When an Friend is deleted, also delete any associated Posts
-      Friend.hasMany(models.Schedule, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return Friend;
+    return User;
   };
   
