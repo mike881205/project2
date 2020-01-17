d67da739ffb90e1eba0113ac857282b770a2e9de
mike881205 <mike881205@gmail.com>
2020-01-16 19:09:54 -0500

    removed handlebars folder, updated HTML routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
+const db = require("../models");
 
-var db = require("../models");
-const path = require("path")
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-
+  },
+  // Render 404 page for any unmatched routes
+  app.get("*", function (req, res) {
+    res.render("404");
+  }));
 };
