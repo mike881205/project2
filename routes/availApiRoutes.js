13dfc85c83253a9e130bf00cb441831f1f176f63
mike881205 <mike881205@gmail.com>
2020-01-18 09:43:39 -0500

    updated index HTML

--- routes/availApiRoutes.js
+++ routes/availApiRoutes.js
@@ -23,11 +23,10 @@ module.exports = function (app) {
       day1: req.body.day1,
       day2: req.body.day2,
       day3: req.body.day3,
-      day4: req.body.thu,
+      day4: req.body.day4,
       day5: req.body.day5,
       day6: req.body.day6,
-      day7: req.body.day7,
-      UserId: 1
+      day7: req.body.day7
     }).then(function (dbAvailability) {
       res.json(dbAvailability);
     })
@@ -44,15 +43,14 @@ module.exports = function (app) {
         day1: req.body.day1,
         day2: req.body.day2,
         day3: req.body.day3,
-        day4: req.body.thu,
+        day4: req.body.day4,
         day5: req.body.day5,
         day6: req.body.day6,
-        day7: req.body.day7,
+        day7: req.body.day7
       },
       {
         where: {
-          id: req.params.UserId
-        }
+          id: req.par      }
       }
     ).then(function (dbAvailability) {
       res.json(dbAvailability);
